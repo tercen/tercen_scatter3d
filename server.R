540224152cc3d0c625168a91eb3229bbc501ad56
Rik <rdewijn@xs4all.nl>
2020-12-17 18:52:33 +0000

    Interactive axes and color

--- server.R
+++ server.R
@@ -20,30 +20,73 @@ getCtx <- function(session) {
 ############################################
 
 server <- shinyServer(function(input, output, session) {
+  ctx = getCtx(session)
+  clist = ctx$cselect() %>% 
+    unite("clist") %>% 
+    mutate(cname = as.character( (1:n())-1))
   
-  what_cols = reactive({
-    c("0", "1", "2")
+  updateSelectInput(session, "X", choices = clist$clist, selected = clist$clist[1])
+  updateSelectInput(session, "Y", choices = clist$clist, selected = clist$clist[2])
+  updateSelectInput(session, "Z", choices = clist$clist, selected = clist$clist[3])
+  
+  if(length(ctx$colors) > 0){
+    clr = data.frame(clist = unlist(ctx$colors)) %>%
+      mutate(cname = as.character( (1:n())-1))
+    updateSelectInput(session, "ColourBy", choices = clr$clist, selected = clr$clist[1])
+  }
+  
+  whatx = reactive({
+    if (input$X == ""){
+      clist$cname[1]
+    } else {
+      slist = clist %>% filter(input$X == clist)
+      slist$cname
+    }
+  })
+  
+  whaty = reactive({
+    if (input$Y == ""){
+      clist$cname[2]
+    } else {
+      slist = clist %>% filter(input$Y == clist)
+      slist$cname
+    }
+  })
+  
+  whatz = reactive({
+    if (input$Z == ""){
+      clist$cname[3]
+    } else {
+      slist = clist %>% filter(input$Z == clist)
+      slist$cname
+    }
+  })
+  
+  whatclr = reactive({
+    if (input$ColourBy == ""){
+      clr$clist[1]
+    } else {
+      input$ColourBy
+    }
   })
   
   xyz = reactive({
-    cxyz = what_cols()
-    ctx = getCtx(session)
     if(length(ctx$colors) > 0){
       df = ctx %>%
         select(.ri,.ci,.y) %>%
         bind_cols(ctx$select(ctx$colors))
       val = df %>%
         dcast(.ri ~ .ci, value.var = ".y") %>% 
-        select(X = any_of(cxyz[1]), Y = any_of(cxyz[2]), Z = any_of(cxyz[3]))
+        select(X = whatx(), Y = whaty(), Z = whatz())
       clr = df %>% 
-        dcast(.ri ~ .ci, value.var = ctx$colors[[1]]) %>%
-        select(clr = any_of("0"))
+        dcast(.ri ~ .ci, value.var = whatclr()) %>%
+        select(clr = "0")
       result = val %>%bind_cols(clr)
     } else {
       result = ctx %>%
         select(.ri,.ci,.y) %>%
         dcast(.ri ~ .ci, value.var = ".y") %>% 
-        select(X = any_of(cxyz[1]), Y = any_of(cxyz[2]), Z = any_of(cxyz[3])) %>%
+        select(X = whatx(), Y = whaty() , Z = whatz()) %>%
         mutate(clr = ".")
     }
   })
@@ -53,6 +96,4 @@ server <- shinyServer(function(input, output, session) {
     fig = plot_ly(df, x = ~X, y = ~Y, z = ~Z, color = ~clr)
     fig = fig %>% add_markers()
   })
-  
 })
-
--- ui.R
+++ ui.R
@@ -2,17 +2,17 @@ library(shiny)
 library(plotly)
 library(tidyr)
 
-shinyUI(fluidPage(
+ui <- shinyUI(fluidPage(
   
   titlePanel("Scatter 3D"),
   
   sidebarPanel(
     selectInput("X", "X", choices = ""),
     selectInput("Y", "Y", choices = ""),
-    selectInput("Z", "Z", choices = "")
-    
+    selectInput("Z", "Z", choices = ""),
+    selectInput("ColourBy", "Colour by", choices = ""),
+    width = 3
   ),
-  
   mainPanel(
     plotlyOutput("sp", height = "600px")
   )
