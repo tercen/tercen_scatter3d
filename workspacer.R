ab5a9f96390a27e5a2f2e7b1ab12b87a4123a15e
Rik <rdewijn@xs4all.nl>
2021-03-17 20:17:44 +0000

    ++

--- workspacer.R
+++ workspacer.R
@@ -11,4 +11,4 @@ source("server.R")
 options("tercen.workflowId"= "8f17d834dda49eba43ac822ed600aa7b")
 options("tercen.stepId"= "706ac7cf-8dd6-41a9-83a7-b5802ea67031")
 
-runApp(shinyApp(ui, server))  
+#runApp(shinyApp(ui, server))  
